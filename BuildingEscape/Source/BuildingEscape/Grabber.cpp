126ff491515a64523e0e5fc0e3087cf73a0cd7cb
DESKTOP-53H1DBN\larsm <lars.magnus.nyland@gmail.com>
2017-01-11 21:13:14 +0100

    Ray-Trace Hit PhysicsBody

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -4,6 +4,8 @@
 #include "Grabber.h"
 
 
+#define OUT
+
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
@@ -30,5 +32,49 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
+
+	// Get the player viewpoint this tick
+	FVector Location = FVector(0.0f, 0.0f, 0.0f);
+	FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f);
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT Location, OUT Rotation);
+
+	/*UE_LOG(LogTemp, Warning, TEXT("Player location: %s, rotation: %s"), *Location.ToString(), *Rotation.ToString());
+	*/
+
+	
+
+	FVector LineTraceEnd = Location + Rotation.Vector()*Reach;
+
+	// Draw a red trace in the world to visualize
+	DrawDebugLine(
+		GetWorld(),
+		Location,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0,
+		1.0f
+	);
+
+	// Setup Query Parameters...
+	FCollisionQueryParams QueryParameters = FCollisionQueryParams(FName(TEXT("")), false, GetOwner());
+
+	// Line-trace ( AKA Ray-Cast ) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		Location,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		QueryParameters
+	);
+	// See what we hit
+	AActor* ObjectHit = Hit.GetActor();
+
+	if (ObjectHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Line Trace hit: %s"), *ObjectHit->GetName())
+	}
 }
 
