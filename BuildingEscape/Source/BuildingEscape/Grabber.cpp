8bc7ccf5abc48291bb733d5e3bc7ec54cdbac128
DESKTOP-53H1DBN\larsm <lars.magnus.nyland@gmail.com>
2017-01-11 18:48:20 +0100

    Setting up the world-BP

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -21,7 +21,7 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
 }
 
 
@@ -30,6 +30,5 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
 }
 
